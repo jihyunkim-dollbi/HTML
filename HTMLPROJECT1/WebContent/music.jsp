57bc7326b0544a3c02a126ccb48a7acd03a8779c
sist <sist@DESKTOP-RVDJDNC>
2020-02-03 18:29:01 +0900

    20200203

---
+++ HTMLPROJECT1/WebContent/music.jsp
@@ -0,0 +1,84 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" import="java.util.*,com.sist.dao.*"%>
+    
+    <%
+    
+    MusicDAO dao = new MusicDAO();
+    ArrayList<MusicVO> list=dao.musiclistData();
+    
+    %>
+    
+    
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<style type="text/css">
+.row {
+margin: 0px auto;
+width: 950px;
+}
+</style>
+
+
+
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<div class="container"></div>
+<h1 class="text-center">지니뮤직의 탑 10</h1>
+<div class="row">
+<table class="table table=hover">
+<tr class="success">
+<th class="text-center">순위</th>
+<th class="text-center"></th>
+<th class="text-center"></th>
+<th class="text-center">곡명</th>
+<th class="text-center">가수명</th>
+</tr>
+
+<%
+
+for(int i=0; i<=50 ; i++)
+{
+	MusicVO vo=list.get(i);
+	String s="";
+	if(vo.getState().equals("유지"))
+		s="-";
+	
+	if(vo.getState().equals("상승"))
+		s="<font color=red>▲</font>";
+	if(vo.getState().equals("하강"))
+		s="<font color=blue>▼</font>";
+	
+	
+
+	
+	%>
+	
+	<tr>
+	<td class="text-center"><%=vo.getMno() %></td>
+	<td class="text-center"><%=s+" "+(vo.getIdcliment()!=0?vo.getIdcliment():" ") %></td>
+	<td class="text-center"><img src="<%=vo.getPoster() %>"width=30 height=30></td>
+	
+	
+	
+	
+	<td><%=vo.getTitle() %></td>
+	<td><%=vo.getSinger() %></td>
+	</tr>
+	
+	<%
+	
+	
+	
+}
+
+%>
+
+</table>
+</div>
+</div>
+</body>
+</html>
---
+++ HTMLPROJECT1/src/com/sist/dao/MusicDAO.java
@@ -0,0 +1,176 @@
+package com.sist.dao;
+
+import java.sql.*;
+import java.util.*;
+
+
+
+public class MusicDAO {
+
+	private Connection conn; 
+	private PreparedStatement ps; 
+	
+	
+	private final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+	
+
+	public MusicDAO()
+	{
+		try 
+		{
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+			
+		}catch(Exception ex) 
+		{
+			
+		ex.printStackTrace();	
+			
+		}
+		
+		
+	}
+	
+	
+	public void getConnection()
+	{
+		
+		try
+		{
+		
+			conn=DriverManager.getConnection(URL, "hr", "happy");
+			
+			
+		}catch(Exception ex) {}
+		
+	}
+	
+	
+
+	public void disConnection()
+	{
+		
+		try
+		{
+			if(ps!=null) ps.close();
+			if(conn!=null) conn.close();
+			
+			
+		}catch(Exception ex) {}
+		
+	}
+	
+	
+	
+	public void musicInsert(MusicVO vo) 
+	{
+		
+		
+		
+		try 
+		{
+			
+			getConnection();
+			
+			String sql = "INSERT INTO music_genie VALUES("   // music_genie = *   music_genie(mno)
+						+"(SELECT NVL(MAX(mno)+1, 1) FROM music_genie),?,?,?,?,?,?,?,?)"; // SUBQUERY
+			
+			// INSERT INTO music_genie VALUES(        )
+		
+			ps=conn.prepareStatement(sql);
+			
+		/*
+		    private int rank;
+			private String title;
+			private String singer;
+			private String album;
+			private String poster;
+			private int idcliment;
+			private String state;
+			private String key;
+
+		 */
+			
+			ps.setInt(1, vo.getRank());
+			ps.setString(2, vo.getTitle());  
+			ps.setString(3, vo.getSinger());
+			ps.setString(4, vo.getAlbum());
+			ps.setString(5, vo.getPoster());
+			ps.setInt(6, vo.getIdcliment());
+			ps.setString(7, vo.getState());
+			ps.setString(8, vo.getKey());
+			
+			
+			
+			ps.executeUpdate();
+			
+			
+			
+			
+		}
+		catch(Exception ex) 
+		{
+			
+			ex.printStackTrace(); 
+			
+		}
+		finally
+		{
+		
+			disConnection();
+			
+		}
+		
+		
+		
+	}
+	
+	
+	public ArrayList<MusicVO> musiclistData()
+	{
+		
+		ArrayList<MusicVO> list= new ArrayList<MusicVO>();
+		try
+		{
+			
+			getConnection();
+			String sql= "SELECT mno,title,singer,poster,album, idcliment,state FROM music_genie "
+					+"ORDER BY rank ASC";
+			ps=conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next())
+			{
+				
+				MusicVO vo = new MusicVO();
+				vo.setMno(rs.getInt(1));
+				vo.setTitle(rs.getString(2));
+				vo.setSinger(rs.getString(3));
+				vo.setPoster(rs.getString(4));
+				vo.setAlbum(rs.getString(5));
+				vo.setIdcliment(rs.getInt(6));
+				vo.setState(rs.getString(7));
+				
+				list.add(vo);
+				
+			}
+			rs.close();
+			
+		}catch(Exception ex)
+		{
+			
+			ex.printStackTrace();
+			
+		}finally
+		{
+			
+			disConnection();
+			
+		}
+		
+		return list;
+	}
+	
+	
+	
+}
